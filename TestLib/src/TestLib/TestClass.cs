85c86307fb61dab5cfc4add5b54b56318c47b58d
Jamie A Hankins <jamiehankins@hotmail.com>
2021-06-10 15:07:16 -0700

    Initial check-in

---
+++ TestLib/src/TestLib/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestLib
+{
+    /// <summary>
+    /// This is a test class to have something for DocFX to document.
+    /// </summary>
+    public class TestClass
+    {
+        /// <summary>
+        /// This is a constructor to document.
+        /// </summary>
+        public TestClass() { }
+
+        /// <summary>
+        /// Simple method to generate docs for.
+        /// </summary>
+        /// <remarks>
+        /// I'd like to take a moment to thank all of those who helped me get to
+        /// a place where I can write documentation like this.
+        /// </remarks>
+        /// <param name="source">A meaningless boolean value, much like most questions in the world.</param>
+        /// <returns>An exactly equivalently meaningless boolean value, much like most answers in the world.</returns>
+        public bool BoolReturningMethod(bool source) => source;
+    }
+}
